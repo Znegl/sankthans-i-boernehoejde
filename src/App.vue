1614d50f9b199d67defe2085b9dc5c987358d1ee
Thomas Grinderslev <mail@thomasgrinderslev.dk>
2024-05-29 19:46:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import MainMap from "@/components/MainMap.vue";
+</script>
+
+<template>
+  <main>
+    <h1>Sankthans i Børnehøjde</h1>
+    <p>Nivås børnevenlige Sankthans-arrangement.</p>
+    <p><time datetime="2024-06-23">Søndag d. 23. juni</time> kl. <time datetime="16:30">16.30</time> til <time datetime="19:00">19.00</time></p>
+    <MainMap/>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 2rem;
+  text-align: center;
+}
+
+.main-map {
+  margin: 2rem -2rem 0;
+  width: calc(100% + 4rem);
+}
+</style>
---
+++ src/components/MainMap.vue
@@ -0,0 +1,35 @@
+<script setup>
+import leaflet from 'leaflet'
+import {ref, watch} from "vue";
+import 'leaflet/dist/leaflet.css';
+
+const mapElement = ref(null)
+
+
+watch(mapElement, (mapElement) => {
+  const map = leaflet.map(mapElement).setView([55.94177, 12.51008], 17);
+
+  leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  const fireMarker = leaflet.marker([55.94152, 12.51202]).addTo(map);
+  const infoMarker = leaflet.marker([55.94130, 12.51193]).addTo(map);
+  const toiletMarker = leaflet.marker([55.94118, 12.51185]).addTo(map);
+  const bikeParkingMarker = leaflet.marker([55.94127, 12.51126]).addTo(map);
+
+})
+</script>
+
+<template>
+  <div ref="mapElement" class="main-map"></div>
+</template>
+
+<style scoped>
+.main-map {
+  height: 60vh;
+  width: 100%;
+  background: silver;
+}
+</style>
